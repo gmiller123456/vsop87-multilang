8c64cac94542e47d6305ff742025ede76df544f7
Greg Miller <gmiller@gregmiller.net>
2019-08-09 23:11:57 -0400

    UPdates to javascript example

---
+++ utility/geodetic2cartesian.py
@@ -0,0 +1,21 @@
+import math
+
+wgs84 = (6378137, 298.257223563)
+
+def geodetic_to_geocentric(ellps, lat, lon, h):
+    """
+    Compute the Geocentric (Cartesian) Coordinates X, Y, Z
+    given the Geodetic Coordinates lat, lon + Ellipsoid Height h
+    """
+    a, rf = ellps
+    lat_rad = math.radians(lat)
+    lon_rad = math.radians(lon)
+    N = a / math.sqrt(1 - (1 - (1 - 1 / rf) ** 2) * (math.sin(lat_rad)) ** 2)
+    X = (N + h) * math.cos(lat_rad) * math.cos(lon_rad)
+    Y = (N + h) * math.cos(lat_rad) * math.sin(lon_rad)
+    Z = ((1 - 1 / rf) ** 2 * N + h) * math.sin(lat_rad)
+
+    return X, Y, Z
+
+r=geodetic_to_geocentric(wgs84,38.2464000,274.236400,0)
+print(r)
